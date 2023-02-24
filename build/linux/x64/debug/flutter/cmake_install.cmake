22c09f86f407f887096f3e449bde24d271f55689
Oriol <110051760+oriolgds@users.noreply.github.com>
2023-02-24 20:50:14 +0100

    Testing on ubuntu linux

--- build/linux/x64/debug/flutter/cmake_install.cmake
+++ build/linux/x64/debug/flutter/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /home/kali/Documentos/GitHub/orschat/linux/flutter
+# Install script for directory: /home/oriol/StudioProjects/orschat/linux/flutter
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/home/kali/Documentos/GitHub/orschat/build/linux/x64/debug/bundle")
+  set(CMAKE_INSTALL_PREFIX "/home/oriol/StudioProjects/orschat/build/linux/x64/debug/bundle")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -35,10 +35,5 @@ endif()
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
-endif()
-
-# Set default install directory permissions.
-if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
 endif()
 
